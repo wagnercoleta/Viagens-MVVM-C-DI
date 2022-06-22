3304731271e141e577eb1d1c3648a6537cddd1f4
wagnercoleta <50020069+wagnercoleta@users.noreply.github.com>
2022-06-22 17:45:29 -0300

    add file LibConstants

---
+++ Viagens-MVVM-C-DI/Lib/LibConstants/LibConstants.swift
@@ -0,0 +1,13 @@
+//
+//  LibConstants.swift
+//  Viagens-MVVM-C-DI
+//
+//  Created by Wagner Coleta on 22/06/22.
+//
+
+import Foundation
+
+struct LibConstants {
+    static let libTitleOk = NSLocalizedString("Ok", comment: "")
+    static let libTitleError = NSLocalizedString("Ops, ocorreu um erro", comment: "")
+}
