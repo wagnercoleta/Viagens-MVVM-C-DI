812b9bb5855b13342540f0f5b3d7e5ca15f794a0
wagnercoleta <50020069+wagnercoleta@users.noreply.github.com>
2022-06-23 10:22:15 -0300

    change start app with ListViagemViewController using Coordinator navigation

--- Viagens-MVVM-C-DI/Application/SceneDelegate.swift
+++ Viagens-MVVM-C-DI/Application/SceneDelegate.swift
@@ -18,9 +18,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
+        let appCoordinator: AppCoordinatorProtocol = LibServiceLocator.shared.autoResolve()
+        appCoordinator.start()
+        
         let safeWindow = UIWindow(windowScene: windowScene)
         safeWindow.frame = UIScreen.main.bounds
-        safeWindow.rootViewController = UINavigationController(rootViewController: ViewController())
+        safeWindow.rootViewController = appCoordinator.navigationController
         safeWindow.makeKeyAndVisible()
         
         window = safeWindow
